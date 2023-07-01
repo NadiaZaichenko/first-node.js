4b741974a0df6886b3aa25fd813508e7b304fdcd
NadiaZaichenko <nadyazaichenko884@gmail.com>
2023-07-01 13:04:07 +0300

    add commander options

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const contacts = require("./contacts");
-const argv = require("yargs").argv;
+const {Command} = require("commander");
 
 const invokeAction = async ({action, id, name, email, phone}) => {
     switch (action) {
@@ -15,9 +15,23 @@ const invokeAction = async ({action, id, name, email, phone}) => {
         case "add" : 
         const newContact = await contacts.addContact({name, email, phone})
         return console.log(newContact);
+        default:
+      console.warn('\x1B[31m Unknown action type!');
     }
 }
-invokeAction({action: "list"});
-// invokeAction({action: "get", id: "qdggE76Jtbfd9eWJHrssH"});
-// invokeAction({action: "remove", id: "_WHBsXNRp9w9I2p7IjZ7o"});
-// invokeAction({action: "add", "name": "Djim Caree","email": "lcarre.Cras@nonenimMauris.net","phone": "(972) 451-3098"});
+// invokeAction({action: "list"});
+// // invokeAction({action: "get", id: "qdggE76Jtbfd9eWJHrssH"});
+// // invokeAction({action: "remove", id: "_WHBsXNRp9w9I2p7IjZ7o"});
+// // invokeAction({action: "add", "name": "Djim Caree","email": "lcarre.Cras@nonenimMauris.net","phone": "(972) 451-3098"});
+
+const program = new Command();
+program
+    .option('-a, --action <type>', 'choose action')
+    .option('-i, --id <type>', 'user id')
+    .option('-n, --name <type>', 'user name')
+    .option('-e, --email <type>','user email')
+    .option('-p, --phone <type>','user phone');
+program.parse(process.argv);
+const argv = program.opts();
+
+invokeAction(argv);
